fef956f5ab4040f5e8adfe8be2137fec9bd9c3fe
Iftakhar Husan <iftakharn19@gmail.com>
2023-01-09 12:32:45 +0600

    Add configs

---
+++ configs.py
@@ -0,0 +1,20 @@
+RECURSIVE: bool = False
+
+FILE_EXTENSIONS: list[str] = [
+    "webm",
+    "mpg",
+    "mp2",
+    "mpeg",
+    "mpe",
+    "mpv",
+    "ogg",
+    "mp4",
+    "m4p",
+    "m4v",
+    "avi",
+    "wmv",
+    "mov",
+    "qt",
+    "flv",
+    "swf",
+]
