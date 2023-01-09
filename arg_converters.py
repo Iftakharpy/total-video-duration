2f0b38e897b6c89e55f7228e9879c0149c108b46
Iftakhar Husan <iftakharn19@gmail.com>
2023-01-09 12:33:24 +0600

    Add argument converters

---
+++ arg_converters.py
@@ -0,0 +1,40 @@
+import re
+from pathlib import Path
+import glob
+
+
+def convert_to_path(path: str) -> Path:
+
+    if path.startswith('~'):
+        path = Path(path).expanduser()
+    else:
+        path = Path(path).absolute()
+
+    is_glob_dir = glob.has_magic(str(path))
+
+    if is_glob_dir:
+        return [Path(path) for path in glob.iglob(str(path))]
+    elif path.exists() and path.is_dir():
+        return path
+    raise ValueError("Path does not exist or not a directory!")
+
+def flatten_paths(paths:list[list[Path]|Path]) -> list[Path]:
+    flatten = []
+    for path in paths:
+        if isinstance(path, Path):
+            flatten.append(path)
+        else:
+            flatten.extend(path)
+    return flatten
+
+def convert_to_regular_expression(regular_expression: str) -> re.Pattern:
+    try:
+        regular_expression = re.compile(regular_expression)
+        return regular_expression
+    except Exception as e:
+        print("Regex pattern: ", e)
+        raise ValueError("Invalid regex pattern")
+
+
+def convert_list_to_set(strings: list[str]) -> set[str]:
+    return set(strings)
